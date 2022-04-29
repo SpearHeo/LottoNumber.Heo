81239dc84571daec43bfb365e64d6f67cde50a8d
SpearHeo <101840164+SpearHeo@users.noreply.github.com>
2022-04-29 18:40:57 +0900

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const numForm = document.querySelector("#numForm");
+const greeting1 = document.querySelector("#greeting1");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function Submit(event) {
+  event.preventDefault();
+
+  function lotto(lottoNum) {
+    var lottoNum = [];
+    for (var i = 0; i <= 6; i++) {
+      var n = Math.floor(Math.random() * 45) + 1;
+
+      for (var j in lottoNum) {
+        if (n == lottoNum[j]) {
+          n = Math.floor(Math.random() * 45) + 1;
+        }
+      }
+      lottoNum.push(n);
+    }
+    lottoNum.sort(function (a, b) {
+      return a - b;
+    });
+    return lottoNum;
+  }
+
+  greeting1.innerText = `예상 당첨 번호\n ${lotto()} `;
+  greeting1.classList.remove(HIDDEN_CLASSNAME);
+}
+numForm.addEventListener("submit", Submit);
+
+document.body.style.backgroundImage = `url(gold.png)`;
