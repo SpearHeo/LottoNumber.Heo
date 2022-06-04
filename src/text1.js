37bd46dcd35a7b8cc85d2ea7d4a14137de0edeea
SpearHeo <101840164+SpearHeo@users.noreply.github.com>
2022-06-04 21:41:35 +0900

    Update text1.js

--- src/text1.js
+++ src/text1.js
@@ -3,17 +3,11 @@ const content = "로또 번호 생성기 ver.4.5"
 const text = document.querySelector(".text1")
 let index = 0;
 
- function sleep(delay){ 
-     const start = new Date().getTime(); 
-     while (new Date().getTime() < start + delay); 
-    }
-
-function typing(){
-    text.textContent += content[index++];
-    if(index > content.length){
-        text.textContent = ""
-        index = 0;
-        sleep(30000);
-    }
+function typing() {
+    if (index < content.length) {
+        let txt = content.charAt(index);
+        text.innerHTML += txt;
+        index++;
+        }
 }
-setInterval(typing, 200);
+    setInterval(typing, 200);
